82606e264f6e61ba7d2ba35970560a1161b08176
Kaue Correa Colling <kauecoficial@gmail.com>
2023-10-18 16:17:32 -0300

    quase

--- Alemao/src/App.jsx
+++ Alemao/src/App.jsx
@@ -9,19 +9,42 @@ const App = () => {
 
   return (
     <div className="App">
+
       <header className="App-header">
         <LinguagemBotao />
         <h1>{t("welcome")}</h1>
       </header>
+
       <main>
+
+        <div className="imgCultura">
+          <h1 className="culturaTitulo">
+            {t("culturaTitulo")}
+          </h1>
+        </div>
+
         <div className="culturaDIV">
-          <h1>{t("culturaTitulo")}</h1>
           <div className="culturaDivConteudo">
-            <img src="./img/culturaAlema.jpg"></img>
+            <img src="./img/bebida.jpg"></img>
             <h3>{t("culturaTexto")}</h3>
           </div>
         </div>
+
+        <div className="imgTurismo">
+          <h1 className="turismoTitulo">
+            {t("turismoTitulo")}
+          </h1>
+        </div>
+
+        <div className="turismoDIV">
+          <div className="turismoDivConteudo">
+            <img src="./img/comida.jpg"></img>
+            <h3>{t("turismoTexto")}</h3>
+          </div>
+        </div>
+
       </main>
+
     </div>
   )
 };
